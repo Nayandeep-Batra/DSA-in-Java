c431cd5caf771e2b24b9c33efb9bfa21bb0c2a9a
Nayandeep-Batra <nayandeepbatra2006@gmail.com>
2026-06-20 00:49:17 +0530

    Added 2D Array Problems

---
+++ 2D_ArrayDSA/SortedMatrixSearch.java
@@ -0,0 +1,24 @@
+public class SortedMatrixSearch { 
+    public static boolean searchMatrix(int matrix[][], int target) {
+        int row = 0, col = matrix[0].length-1;
+        while(row < matrix.length && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            }
+            else if(target < matrix[row][col]) {
+                col--;
+            }
+            else {
+                row++;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {{1,3,5,7},
+                        {10,11,16,20},
+                        {23,30,34,60}};
+        int target = 30;
+        System.out.println(searchMatrix(matrix, target));
+    }
+}
